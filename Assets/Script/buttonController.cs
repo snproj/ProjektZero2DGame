6f8d9692fd931a549f64b5b96ee9c5ac9caf0126
Pin Nean Lai <pinnean_lai@mymail.sutd.edu.sg>
2021-03-04 21:18:18 +0800

    hello

---
+++ Assets/Script/buttonController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonController : MonoBehaviour
+{
+    private SpriteRenderer theSR;
+    public Sprite defaultImage;
+    public Sprite pressedImage;
+
+    public KeyCode keyToPress;
+    void Start()
+    {
+        theSR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress))
+        {
+            theSR.sprite = pressedImage;
+        }
+
+        if (Input.GetKeyUp(keyToPress))
+        {
+            theSR.sprite = defaultImage;
+        }
+    }
+}
